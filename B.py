a26f254d770f0eb6514aadac066c03dca81fca64
Tiago <you@example.com>
2024-10-16 17:30:27 +0100

    o meu segundo comit

---
+++ B.py
@@ -0,0 +1,8 @@
+def main ():
+    a = int(input())
+    b = int(input())
+    c = a + b
+    print(c)
+
+if __name__ =="__main__":
+     main()
---
+++ C.py
@@ -0,0 +1,7 @@
+def main():
+    x = input()
+    r = 1/(1+2.718281828459045**(-float(x)))
+    print(r)
+
+if __name__ =="__main__":
+     main()
